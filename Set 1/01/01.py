5a12df6cd99cd10cce5016d86e34ed61dcc49cfe
Crack <amoghpavan5363@gmail.com>
2021-01-30 15:28:48 +0530

    Update 01.py

--- Set 1/01/01.py
+++ Set 1/01/01.py
@@ -1,4 +1,4 @@
-# Implement and demonstratethe FIND-Salgorithm for finding the most specific hypothesis
+# Implement and demonstratethe FIND-S algorithm for finding the most specific hypothesis
 # based on a given set of training data samples. Read the training data from a .CSV file.
 
 import numpy as np
